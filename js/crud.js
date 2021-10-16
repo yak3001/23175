f26cbd071134e17c01a447f4efac8d23e2bfa609
Totxo <tomaschorroarin@gmail.com>
2021-10-16 14:12:56 -0300

    clase 8

--- js/crud.js
+++ js/crud.js
@@ -14,11 +14,11 @@ class Perro {
 }
 
 // Lista de perritos
-let perritos = []
+let perritos = JSON.parse(localStorage.getItem('perritos')) || []
+
 
 // Metodo que retorna la lista de perritos
 const getAll = () => {
-    perritos = JSON.parse(localStorage.getItem('perritos'))
     return perritos;
 }
 
@@ -70,12 +70,15 @@ const perro4 = new Perro('Pacha', 'Marron', 15)
 
 // Paso 2
 // agregar perro1 a la lista
+
+
 /*
 create(perro1)
 create(perro2)
 create(perro3)
 create(perro4)
 */
+
 // Paso 3
 // Busco un perro por su nombre
 // console.log(findOne('molo'))
@@ -89,3 +92,36 @@ create(perro4)
 
 // Obtener la lista completa de perros
 console.log(getAll())
+
+
+// Obtener elmentos del DOM
+const listaPerros = document.getElementById('lista-perros')
+const formPerro = document.getElementById('form-perro')
+const inputNombrePerro = document.getElementById('input-nombre-perro')
+const inputColorPerro = document.getElementById('input-color-perro')
+const inputEdadPerro = document.getElementById('input-edad-perro')
+
+// Agregar perritos a la lista de perros para el browser
+for (let perro of perritos) {
+    console.log(perro)
+    let itemPerro = document.createElement('li')
+    itemPerro.textContent = `El nombre del perro es ${perro.nombre}`
+    listaPerros.appendChild(itemPerro)
+}
+
+// Escuchar el evento submit del formulario
+formPerro.addEventListener('submit', (event) => {
+    // event.preventDefault()
+    // console.log(event)
+    // console.log("aca tengo que agregar el perro")
+
+    const nombre = inputNombrePerro.value
+    const color = inputColorPerro.value
+    const edad = inputEdadPerro.value
+
+    const perro = new Perro(nombre, color, edad)
+
+    create(perro)
+
+
+})
